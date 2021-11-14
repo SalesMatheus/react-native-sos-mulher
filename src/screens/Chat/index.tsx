a5a04ec62d985fbbc6e7f883ddb96f64af03925d
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-11-13 23:04:19 -0300

    feature: adiciona resposta no chat

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -13,8 +13,22 @@ interface MessageProps {
       _id: number;
       name: string;
       avatar: string;
-    }
+    };
+    quickReplies?: QuickReplies
 }
+
+interface Reply {
+    title: string
+    value: string
+    messageId?: any
+}
+
+interface QuickReplies {
+    type: 'radio' | 'checkbox'
+    values: Reply[]
+    keepIt?: boolean
+}
+  
 
 export function Chat() {
   const [messages, setMessages] = useState<MessageProps[]>([]);
@@ -26,7 +40,7 @@ export function Chat() {
         text: 'Polícia Militar. Em que posso ajudar?',
         createdAt: new Date(),
         user: {
-          _id: 2,
+          _id: 1,
           name: 'Polícia Militar',
           avatar: 'https://w7.pngwing.com/pngs/261/536/png-transparent-computer-icons-avatar-police-officer-avatar-heroes-fictional-character-army-officer.png',
         },
@@ -36,15 +50,38 @@ export function Chat() {
 
   const onSend = useCallback((messages = []) => {
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    
+    if(messages[0].text){
+        setMessages(previousMessages => GiftedChat.append(previousMessages, [
+            {
+              _id: Math.random(),
+              text: 'Necessita que uma viatura vá até você?',
+              createdAt: new Date(),
+              user: {
+                _id: 1,
+                name: 'Polícia Militar',
+                avatar: 'https://w7.pngwing.com/pngs/261/536/png-transparent-computer-icons-avatar-police-officer-avatar-heroes-fictional-character-army-officer.png',
+              },
+            },
+          ]))
+    }
   }, [])
 
+  const OnQuickReply = useCallback((quickReplies = []) => {
+    // setMessages(previousMessages => GiftedChat.append(previousMessages, quickReplies))
+    console.log(quickReplies)
+  }, [])
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
-    />
+    <Container>
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            onQuickReply={quickReplies => OnQuickReply(quickReplies)}
+            user={{
+                _id: 2,
+            }}
+        />
+    </Container>
+
   )
 }
